dccc7632b8a09414532379b8cc3f1d95c95e2eb8
Ayush <ayushkgupta007@gmail.com>
2018-11-08 02:05:56 +0530

    middleware added

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectId} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json());
@@ -87,7 +88,10 @@ app.post('/users',(req,res) => {
             console.log('Some error');
             res.status(400).send(err);
         });
-})
+});
+app.get('/users/me',authenticate, (req,res) => {
+    res.send(req.user);
+});
 app.listen(port, () => {
     console.log(`Server running on ${port}.`);
 });
